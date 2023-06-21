f5053f21403615282a4f16596840ccf4059bb54e
Dennis Kimaita <dkimaita22@gmail.com>
2023-06-21 14:58:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    let content=document.getElementById("text")
+    content.textContent="This is really cool!"
+})
